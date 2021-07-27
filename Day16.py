b5819d3bf2ade63a150fe58517031608a5666182
azrvus <azrv.us@gmail.com>
2021-07-27 16:01:58 -0500

    day 16

---
+++ Day16.py
@@ -0,0 +1,8 @@
+from turtle import Turtle, Screen
+rex = Turtle()
+print(rex)
+rex.shape('turtle')
+my_screen = Screen()
+print(my_screen.canvheight)
+rex.forward(100000000)
+my_screen.exitonclick()
